f5800ec5f9301b292b70abb584f0bd2fd938e8fa
Shashi Dhar Kumar <11044107+shashidharkumar@users.noreply.github.com>
2018-08-23 21:01:22 +0530

    Update distancebetweentwolatlng.php

--- distancebetweentwolatlng.php
+++ distancebetweentwolatlng.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>distance between two lat lng</title>
+<title>Calculate distance between two latitude longitude using PHP</title>
 </head>
 
 <body>
